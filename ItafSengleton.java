1c1e3510451bd28aa3cc74ae0bbd7b49928ca828
ItafNoorZahia <117036831+ItafNoorZahia@users.noreply.github.com>
2022-10-31 03:20:44 +0300

    Add files via upload

---
+++ ItafSengleton.java
@@ -0,0 +1,29 @@
+package itaf;
+
+public class ItafSengleton {
+
+				
+ private static ItafSengleton uniqueInstance;
+// other useful instance variable here
+   private ItafSengleton(){} 
+   private String MyName;		 
+ public static ItafSengleton getInstance() {
+		if(uniqueInstance== null) {
+			 uniqueInstance= new ItafSengleton();
+ }
+		return  uniqueInstance;
+  }
+				
+				
+			// other useful methods here
+ public String getMyName() {
+	    return MyName;
+	}
+
+	public void setMyName(String MyName) {
+	    this.MyName = MyName;
+	}
+
+}
+
+
