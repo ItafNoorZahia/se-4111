f10cc0d0d878d82f0d89bd4bd676a10cc5ab9a5a
ItafNoorZahia <117036831+ItafNoorZahia@users.noreply.github.com>
2022-11-01 01:21:57 +0300

    Add files via upload

---
+++ Singleton.java
@@ -0,0 +1,19 @@
+package l;
+
+public class Singleton {
+
+	 private static Singleton uniqueInstance;
+	 // other useful instance variable here
+	  
+	  private Singleton() {} 
+	  
+
+	 public static Singleton getInstance() {
+		 if(uniqueInstance== null) {
+			 uniqueInstance= new Singleton();
+		 }
+		return  uniqueInstance;
+	 }
+
+
+}
