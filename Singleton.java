2f2b9f29230d34d571246cf065bd1c2a4e4be7ad
ItafNoorZahia <117036831+ItafNoorZahia@users.noreply.github.com>
2022-11-07 20:13:05 +0300

    Add files via upload

--- Singleton.java
+++ Singleton.java
@@ -1,25 +1,21 @@
-package l;
+package Singleton;
 
 public class Singleton {
-
-
-	 private static Singleton uniqueInstance;
-	 // other useful instance variable here
-	  
-	  private Singleton() {} 
-	  private String MyNamee; 
-
-
-	 public static Singleton getInstance() {
-		 if(uniqueInstance== null) {
-			 uniqueInstance= new Singleton();
-		 }
-		return  uniqueInstance;
-	 }
-	 public String getMyname() {
-			return MyName;
-		}
-		public void setMyName(String MyName) {
-			this.MyName= MyName;
-		}
+	
+private static Singleton uniqueInstance;
+		 // other useful instance variable here
+private Singleton() {
+	System.out.println("Your name");
+} 
+		  
+		 
+public static Singleton getInstance() {
+    if(uniqueInstance== null) {
+	   uniqueInstance= new Singleton();
+	}
+	return  uniqueInstance;
 }
+}
+
+
+
